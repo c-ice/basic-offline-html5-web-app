57d5f22b13b8b33bb0fc08346ab5ccf7e544a345
c-ice <lukas.chudy@softec.sk>
2012-12-12 18:12:59 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,12 @@
-openDatabase('documents', '1.0', 'Local document storage', 5*1024*1024, function (db) {
-  db.changeVersion('', '1.0', function (t) {
-    t.executeSql('CREATE TABLE docids (id, name)');
-  }, error);
+var db = openDatabase("APP", "1.0", "CoffeeCounter", (5 * 1024 * 1024));
+db.transaction(function (tx) {
+  tx.executeSql("CREATE TABLE IF NOT EXISTS counter (id, user, count)", [ ], successCallback, errorCallback);
+  tx.executeSql("INSERT INTO counter (id, user, count) VALUES(?,?,?)", [1,"username",0], function(){}, errorCallback);
+});
+
+db.transaction(function (tx) {
+  tx.executeSql("SELECT user, count FROM counter WHERE id = ?", [1], 
+    function(data){
+      console.log(data.user + " vypil " + data.count + " kavy.");
+  }, errorCallback);
 });
