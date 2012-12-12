4424c8802f36565e04fa67105e8186dc7f25b870
c-ice <lukas.chudy@softec.sk>
2012-12-12 18:19:25 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,9 @@
 var db = openDatabase("APP", "1.0", "CoffeeCounter", (5 * 1024 * 1024));
 db.transaction(function (tx) {
-  tx.executeSql("CREATE TABLE IF NOT EXISTS counter (id, user, count)", [ ], successCallback, errorCallback);
-  tx.executeSql("INSERT INTO counter (id, user, count) VALUES(?,?,?)", [1,"username",0], function(){}, errorCallback);
+  tx.executeSql("CREATE TABLE IF NOT EXISTS counter (id, user, count)", 
+      [ ], successCallback, errorCallback);
+  tx.executeSql("INSERT INTO counter (id, user, count) VALUES(?,?,?)", 
+      [1,"username",0], function(){}, errorCallback);
 });
 
 db.transaction(function (tx) {
